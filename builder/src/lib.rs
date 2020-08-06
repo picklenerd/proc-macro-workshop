43ca51af928d90ada2687498861bb61196eac4e7
Jordan Grace <jordan.t.grace@gmail.com>
2020-08-06 00:37:40 -0400

    builder-6

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,7 +1,7 @@
 extern crate proc_macro;
 
 use proc_macro::TokenStream;
-use syn::{DeriveInput, parse_macro_input};
+use syn::{Data, Ident, Field, Type, DeriveInput, parse_macro_input};
 use quote::{quote, format_ident};
 
 #[proc_macro_derive(Builder)]
@@ -11,6 +11,32 @@ pub fn derive(input: TokenStream) -> TokenStream {
 
     let struct_name = format_ident!("{}", input.ident);
     let builder_name = format_ident!("{}Builder", input.ident);
+
+    let fields = match input.data {
+        Data::Struct(struct_data) => {
+            struct_data.fields
+                .iter()
+                .map(FieldInfo::from_field)
+                .flatten()
+                .collect()
+        },
+        _ => Vec::new(),
+    };
+
+    let setters: Vec<proc_macro2::TokenStream> = fields
+        .iter()
+        .map(FieldInfo::setter)
+        .collect();
+
+    let validations: Vec<proc_macro2::TokenStream> = fields
+        .iter()
+        .map(FieldInfo::validation)
+        .collect();
+
+    let field_builders: Vec<proc_macro2::TokenStream> = fields
+        .iter()
+        .map(FieldInfo::build)
+        .collect();
 
     let result = quote! {
         pub struct #builder_name {
@@ -32,42 +58,125 @@ pub fn derive(input: TokenStream) -> TokenStream {
         }
 
         impl #builder_name {
-            pub fn executable(&mut self, executable: String) -> &mut Self {
-                self.executable = Some(executable);
-                self
-            }
-
-            pub fn args(&mut self, args: Vec<String>) -> &mut Self {
-                self.args = Some(args);
-                self
-            }
-
-            pub fn env(&mut self, env: Vec<String>) -> &mut Self {
-                self.env = Some(env);
-                self
-            }
-
-            pub fn current_dir(&mut self, current_dir: String) -> &mut Self {
-                self.current_dir = Some(current_dir);
-                self
-            }
+            #(#setters)*
 
             pub fn build(&mut self) -> Result<#struct_name, Box<dyn std::error::Error>> {
-                match (self.executable.clone(), self.args.clone(), self.env.clone(), self.current_dir.clone()) {
-                    (None, _, _, _) => Err("Missing field: executable".into()),
-                    (_, None, _, _) => Err("Missing field: args".into()),
-                    (_, _, None, _) => Err("Missing field: env".into()),
-                    (_, _, _, None) => Err("Missing field: current_dir".into()),
-                    (Some(executable), Some(args), Some(env), Some(current_dir)) => Ok(#struct_name {
-                        executable,
-                        args,
-                        env,
-                        current_dir,
-                    }),
+                let mut missing_fields = Vec::new();
+
+                #(#validations)*
+
+                if missing_fields.is_empty() {
+                    Ok(#struct_name {
+                        #(#field_builders)*
+                    })
+                } else {
+                    Err(format!("Missing fields: {}", missing_fields.join(",")).into())
                 }
             }
         }
     };
 
     result.into()
+}
+
+struct FieldInfo {
+    pub ident: Ident,
+    pub inner_type: Type,
+    pub is_required: bool,
+}
+
+impl FieldInfo {
+    pub fn from_field(field: &Field) -> Option<Self> {
+        match get_option_type(field) {
+            Some((ident, inner_type)) => {
+                Some(Self {
+                    ident,
+                    inner_type,
+                    is_required: false,
+                })
+            }
+            None => {
+                Some(Self {
+                    ident: field.ident.clone()?,
+                    inner_type: field.ty.clone(),
+                    is_required: true,
+                })
+            },
+        }
+    }
+
+    pub fn setter(&self) -> proc_macro2::TokenStream {
+        let parameter_name = &self.ident;
+        let parameter_type = &self.inner_type;
+
+        quote! {
+            pub fn #parameter_name(&mut self, #parameter_name: #parameter_type) -> &mut Self {
+                self.#parameter_name = Some(#parameter_name);
+                self
+            }
+        }
+    }
+
+    pub fn validation(&self) -> proc_macro2::TokenStream {
+        if !self.is_required {
+            return proc_macro2::TokenStream::new();
+        }
+
+        let parameter_name = &self.ident;
+
+        quote! {
+            if self.#parameter_name.is_none() {
+                missing_fields.push(stringify!(#parameter_name));
+            }
+        }
+    }
+
+    pub fn build(&self) -> proc_macro2::TokenStream {
+        let parameter_name = &self.ident;
+
+        if self.is_required {
+            quote! {
+                #parameter_name: self.#parameter_name.clone().unwrap(),
+            }
+        } else {
+            quote! {
+                #parameter_name: self.#parameter_name.clone(),
+            }
+        }
+    }
+}
+
+fn get_option_type(field: &Field) -> Option<(Ident, Type)> {
+    use syn::{Path, TypePath, PathArguments, GenericArgument};
+
+    let ident = field.ident.as_ref()?;
+
+    match &field.ty {
+        Type::Path(
+            TypePath {
+                qself: None,
+                path: Path {
+                    leading_colon: None,
+                    segments,
+                },
+            },
+        ) => {
+            let segment = segments.iter().next()?;
+            if segment.ident == "Option" {
+                if let PathArguments::AngleBracketed(args) = &segment.arguments {
+                    let arg = args.args.iter().next()?;
+                    if let GenericArgument::Type(arg_type) = arg {
+                        Some((ident.clone(), arg_type.clone()))
+                    } else {
+                        None
+                    }
+                } else {
+                    None
+                }
+            } else {
+                None
+            }
+        },
+        _ => None,
+    }
 }
